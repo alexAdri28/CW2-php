6683dfb698a2de3a09b99cc8e6967822559055cb
alexAdri28 <rio_kevin1999@hotmail.com>
2022-05-18 00:42:40 +0100

    uptades some bugs on the login page

--- templates/login.php
+++ templates/login.php
@@ -3,13 +3,13 @@
 
 <form name="frmLogin" action="authenticate.php" method="post">
   <div class="mb-3">
-    <label class="form-label">Student ID </label>
-    <input type="txt" class="form-control" id="txtid"name="txtid">
+    <label class="txtid"class="form-control">Student ID </label>
+    <input type="text" class="form-control" id="txtid"name="txtid">
   
-  <div class="mb-3">
+   <div class="mb-3">
     <label for="txtpwd"class="form-label">Password</label>
-    <input type="{Password}" class="form-control" id="txtpwd"name="txtid">
+    <input type="Password"class="form-control" id="txtpwd"name="txtpwd">
   </div>
-  </div>
-  <input ="submit" name="btnlogin" class="btn btn-primary" value="Login"/>
+  
+  <input type="submit"name="btnlogin" class="btn btn-primary" value="Login"/>
 </form>
