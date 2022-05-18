215d0105702466af0e889377ef27d6cb0a0106e2
alexAdri28 <rio_kevin1999@hotmail.com>
2022-05-18 11:19:28 +0100

    changes

--- task 1 .php
+++ task 1 .php
@@ -3,17 +3,10 @@
    include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
-
-  
-
    
-   // check wheather we are logged in 
-   if (isset($_SESSION['id'])) 
-   {
-       $faker = Faker\Factory::create();
-
-
-   for ($i = 0; $i <5; $i++)
+  require_once 'vendor/autoload.php';
+  
+       for ($i = 0; $i <5; $i++)
    {
       $studentid = $faker->numberBetween(20000001, 99999999);
       $password = password_hash($faker->password(), PASSWORD_DEFAULT);
@@ -21,21 +14,26 @@
       $firstname = $faker->firstName($gender = null | 'male'|'female');
       $lastname = $faker->lastName();
       $streetAdress = $faker->streetAddress();
-      $town = "London";
-      $county = "Greater London";
+      $town = "High Wycombe";
+      $county = "Buckinghamshrie";
       $country = "United Kingdom";
-      $postcode =  "SW1" . $faker->numberBetween(0, 1) . " " . 
+      $postcode =  "HP1" . $faker->numberBetween(0, 1) . " " . 
       $faker->randomNumber(2, false) . 
       strtoupper($faker->randomLetter()) . strtoupper($faker->randomLetter());
 
-      $sql = "INSERT INTO student (studentid, password, DOB, firstname, lastname, house, town, county, country, postcode)
+      $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
       VALUES ('$studentID', '$password', '$dob', '$firstName', '$lastName', '$streetAdress', '$town', '$county', '$country',
        '$postCode')";
-      
-      $result = mysqli_query($conn, $sql);
-    }
-  
+
+       $result = mysqli_query($conn, $sql);
    }
+
+   if (isset($_GET['return'])) {
+    }
+		 
+
+?>
+
 
    
 
--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -4,8 +4,9 @@
 <nav class="nav">
   <a class="nav-link active" aria-current="page" href="modules.php">My Modules</a>|
   <a class="nav-link" href="Task4.php">Add Student</a> |
-  <a class="nav-link" href="Insert_student.php">Insert Student</a> |
-   <a class="nav-link" href="assignmodule.php">assignmodule</a> |
-    <a class="nav-link" href="details.php">details</a> |
-    <a class="nav-link" href="logout.php">Logout</a> |
+  <a class="nav-link" href="Task 1. php">Insert Student</a> |
+  <a class="nav-link" href="assignmodule.php">assignmodule</a> |
+  <a class="nav-link" href="Task2.php">Student</a> |
+
+  <a class="nav-link" href="logout.php">Logout</a> |
 </nav>
