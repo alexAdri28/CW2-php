24641967d95d3a2b0f9608a2d14ce1dcf4f7a380
alexAdri28 <rio_kevin1999@hotmail.com>
2022-05-18 12:34:25 +0100

    update comple work

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -10,7 +10,7 @@ include("js/index.html");
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
    <a href="student.php">Existing Student</a> |
-    <a href="addstudent.php">Add New Student</a> |
+    <a href="addstudent.hmtl">Add New Student</a> |
     <a href="studentdelete.php">Delete Student Record</a> |
    <a href="logout.php">Logout</a>
   </head>
